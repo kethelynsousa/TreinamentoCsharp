1a2e96b5f2dd5955c86d3e7b2de29f17d4959cee
Aluno <Kethelyn@aluno.senai.br>
2022-07-04 11:37:07 -0300

    vaipoha

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoGitHub.InterfaceConsole
+{ // inicio do namespace
+    internal class Program
+    { //inicio da classe
+        static void Main(string[] args)
+        { //inicio do métado
+            /*
+             * Main - principal
+             * O métado inicia -se com a letra maíscula
+             * 
+             */
+    
+            Console.WriteLine("alô Mundo!");
+            Console.ReadKey();
+
+        } //fim do métado
+    } //fim da classe
+} //fim do namespace
